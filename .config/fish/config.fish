0cb03fefa2c6c405cb7937df8466c88c26714652
Kotob <asheraryam@gmail.com>
2020-12-16 22:35:46 +0300

    Update config.fish

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,8 +1,12 @@
+contains $HOME/bin $PATH ; or set PATH $HOME/bin $PATH
+contains $HOME/.cargo/bin $PATH ; or set PATH $HOME/.cargo/bin $PATH
+contains $PATH:$HOME/.config/itch/apps/butler $PATH ; or set PATH $PATH:$HOME/.config/itch/apps/butler $PATH
+
+# Deduplicate paths. Adapted from https://github.com/fish-shell/fish-shell/issues/5117#issuecomment-408639996
+set -x PATH (printf '%s\n' $PATH | sort -u)
 
 alias vi=nvim
 alias arrr='sudo protonvpn c --p2p'
 alias hatab='sudo protonvpn c US-GA#14'
-
-
 
 starship init fish | source
